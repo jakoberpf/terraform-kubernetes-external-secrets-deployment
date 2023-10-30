093a933d8f316c9a6e7a13ac2c1120e56048785c
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-10-30 18:10:23 +0100

    even more formatting

--- external-secrets.tf
+++ external-secrets.tf
@@ -4,22 +4,18 @@ resource "helm_release" "external_secrets" {
   chart      = "external-secrets"
   version    = var.helm_chart_version
   namespace  = var.namespace
-
   set {
     name  = "replicaCount"
     value = "3"
   }
-
   set {
     name  = "leaderElect"
     value = "true"
   }
-
   set {
     name  = "installCRDs"
     value = "true"
   }
-
   depends_on = [
     kubernetes_namespace.external_secrets,
   ]
