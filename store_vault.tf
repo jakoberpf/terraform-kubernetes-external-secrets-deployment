f8b18465110819df315ca32615aa16212a0c46ef
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-10-30 18:10:08 +0100

    formatting

--- store_vault.tf
+++ store_vault.tf
@@ -1,15 +1,12 @@
 data "kubectl_path_documents" "store_vault" {
   pattern = "${path.module}/kubernetes/store-vault.store.tpl"
-
   vars = {
     namespace = var.namespace
     server    = var.vault_server
   }
-
   sensitive_vars = {
     server = var.vault_server
   }
-
   depends_on = [
     helm_release.external_secrets,
   ]
@@ -21,16 +18,13 @@ resource "kubectl_manifest" "store_vault" {
 
 data "kubectl_path_documents" "store_vault_secret" {
   pattern = "${path.module}/kubernetes/store-vault.secret.tpl"
-
   vars = {
     namespace = var.namespace
     token     = var.vault_token
   }
-
   sensitive_vars = {
     token = var.vault_token
   }
-
   depends_on = [
     helm_release.external_secrets,
   ]
@@ -38,7 +32,6 @@ data "kubectl_path_documents" "store_vault_secret" {
 
 resource "kubectl_manifest" "store_vault_secret" {
   yaml_body = data.kubectl_path_documents.store_vault_secret.documents[0]
-
   sensitive_fields = [
     "stringData.token"
   ]
